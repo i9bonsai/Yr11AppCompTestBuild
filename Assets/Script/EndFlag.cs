7246a886803b3771533e662d469a98b38fc2b7a0
i9bonsai <kupa.vx@gmail.com>
2022-08-30 12:34:23 +1000

    Level 2 Changes

--- Assets/Script/EndFlag.cs
+++ Assets/Script/EndFlag.cs
@@ -14,7 +14,7 @@ public class EndFlag : MonoBehaviour
         {
             if (lastLevel == true)
             {
-                SceneManager.LoadScene(0);
+                SceneManager.LoadScene(2);
             }
             else
             {
